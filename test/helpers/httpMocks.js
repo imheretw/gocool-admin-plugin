f2e5d232d852ec0ffee80041f2cc7aa331cade67
Kos <koshuang@gmail.com>
2017-04-26 08:51:20 +0800

    feat: basic plguin

---
+++ test/helpers/httpMocks.js
@@ -0,0 +1,7 @@
+import nock from 'nock';
+import { Logger } from 'gocool';
+
+const logger = new Logger('httpMock');
+
+export default {
+};
---
+++ test/services/UserService.spec.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import UserService from '../../lib/services/UserService';
+import httpMocks from '../helpers/httpMocks';
+import githubClosedIssueData from '../data/github/issues/closed.json';
+
+describe('Test UserService', () => {
+  describe('constructor()', () => {
+    it('should return a object', async () => {
+    });
+  });
+});
